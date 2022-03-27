a826a89068dcd189b3647654d62a06680d98f838
congzheh <34508191+congzheh@users.noreply.github.com>
2022-03-27 11:08:54 +0800

    Create db_login.php

---
+++ db_login.php
@@ -0,0 +1,8 @@
+<?php
+///数据库连接信息
+$db_Type = "mysql";                                 //数据库类型
+$host = "localhost";                          //数据库地址
+$dbName = "xna";                                    //数据库名
+$userName = "xna";                                  //数据库用户名
+$password = "xna";                     //数据库密码
+$dsn = "{$db_Type}:host={$host};dbname={$dbName}";  //连接字符串
